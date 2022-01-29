d934a959de00fbcb5ce620392a2af75d86be3184
henriklagergren <henriklagergren@hotmail.com>
2022-01-29 15:37:41 +0100

    added change of header on scroll

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,21 +1,24 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import logo from "./assets/images/devkey_logo.png"
 
-const Wrapper = styled.div`
+
+const Wrapper = styled.div<{activeHeader: boolean}>`
     width: 100vw;
     height: 74px;
     display: flex;
     box-sizing: border-box;
-    background-color: transparent;
+    background-color: ${props => props.activeHeader ? 'white' : 'transparent'};
     justify-content: space-between;
     position: fixed;
     top: 0;
 `;
 
-const Logo = styled.img`
+const Logo = styled.img<{activeHeader: boolean}>`
     height: calc(100% - 20px);
     width: auto;
     padding: 10px;
+    filter: ${props => props.activeHeader ? 'invert(1)' : 'none'};
 `;
 
 const HeaderRow = styled.ul`
@@ -27,22 +30,42 @@ const HeaderRow = styled.ul`
     align-items: center;
 `;
 
-const RowItem = styled.li`
-    color: white;
+const RowItem = styled.li<{activeHeader: boolean}>`
+    color: ${props => props.activeHeader ? 'black' : 'white'};
     text-transform: uppercase;
     list-style: none;
 `;
 
 
 
+
 function Header() {
-    return <Wrapper>
-        <Logo src={logo} />
+    const [activeHeader,setActiveHeader] = useState(window.scrollY > 60);
+
+    useEffect(()=>{
+    let scrollListener = () => {
+        let isActive = window.scrollY < 60;
+        if (isActive && activeHeader) {
+            setActiveHeader(false)
+          } else if (!isActive && !activeHeader){
+            setActiveHeader(true)
+          }
+      };
+
+        window.addEventListener("scroll",scrollListener)
+
+        return () => {
+            window.removeEventListener("scroll", scrollListener);
+          };
+    },[activeHeader]);
+
+    return <Wrapper activeHeader={activeHeader}>
+        <Logo src={logo} activeHeader={activeHeader}/>
         <HeaderRow>
-            <RowItem>Hem</RowItem>
-            <RowItem>Varför devkey?</RowItem>
-            <RowItem>Våra Projekt</RowItem>
-            <RowItem>Kontakt</RowItem>
+            <RowItem activeHeader={activeHeader} >Hem</RowItem>
+            <RowItem activeHeader={activeHeader}>Varför devkey?</RowItem>
+            <RowItem activeHeader={activeHeader}>Våra Projekt</RowItem>
+            <RowItem activeHeader={activeHeader}>Kontakt</RowItem>
         </HeaderRow>
     </Wrapper>
 }
