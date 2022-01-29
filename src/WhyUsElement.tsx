6ee48e1d5687c237e2423356099427f72b907431
henriklagergren <henriklagergren@hotmail.com>
2022-01-29 12:31:58 +0100

    added FeatureItems to WhyUsElement

--- src/WhyUsElement.tsx
+++ src/WhyUsElement.tsx
@@ -5,17 +5,20 @@ import ScheduleIcon from './assets/images/schedule.svg';
 import SmartphoneIcon from './assets/images/smartphone.svg';
 import SpeedIcon from './assets/images/speed.svg';
 import HotIcon from './assets/images/whatshot.svg';
+import FeatureItem from "./FeatureItem";
 
 const BackgroundWrapper = styled.div`
     
 `;
 
 const Title = styled.h2`
-    
+    text-align: center;
+    font-size: 30px;
 `;
 
 const Body = styled.h3`
-    
+    text-align: center;
+    font-size: 16px;
 `;
 
 const Grid = styled.div`
@@ -28,15 +31,21 @@ const Grid = styled.div`
 
 
 
-function WhyUsComponent() {
+function WhyUsElement() {
     return (
         <BackgroundWrapper>
-            <Title></Title>
-            <Body></Body>
+            <Title>Varför devKey?</Title>
+            <Body>Det finns flera anledningar till att välja devKey som apputvecklare:</Body>
             <Grid>
+            <FeatureItem title="Både Android & iOS" body="Alla våra appar utvecklas i Googles populära ramverk Flutter, vilket möjliggör en kodbas för både iOS & Android." icon={SmartphoneIcon} />
+            <FeatureItem title="Kort utvecklingstid" body="Hantering av endast en kodbas tillsammans med förbyggda komponenter minskar utvecklingstiden." icon={BoltIcon} />
+                <FeatureItem title="Låg kostnad" body="Kortare utvecklingstid och vår erfarenhet av ramverket bidrar till lägre kostnader." icon={SavingsIcon} />
+                <FeatureItem title="Bra prestanda" body="Appar skrivna i Flutter presterar bättre än sina konkurrenter, vilket i sin tur höjer användarupplevelsen." icon={SpeedIcon} />
+                <FeatureItem title="Modernt gränssnitt" body="Vi använder oss av Material UI, ett gränssnitt som ligger helt rätt i tiden." icon={HotIcon} />
+                <FeatureItem title="Stödjer äldre mobiler" body="Våra appar ser exakt lika ut på alla enheter, även äldre mobiler." icon={ScheduleIcon} />
             </Grid>
         </BackgroundWrapper>
     )
 }
 
-export default WhyUsComponent;
+export default WhyUsElement;
