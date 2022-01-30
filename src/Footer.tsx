bed44f115152f801404b6daf1b4b3093245d4ea1
henriklagergren <henriklagergren@hotmail.com>
2022-01-30 14:08:46 +0100

    added footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,12 +1,41 @@
 import styled from "styled-components";
+import DevkeyLogo from "./assets/images/devkey_logo.png";
 
 const Wrapper = styled.div`
-  height: 500px;
+  padding: 20px;
+  padding-top: 60px;
   background-color: #023047;
 `;
 
+const CompanyIcon = styled.img`
+  width: 130px;
+`;
+
+const Body = styled.h2`
+  width: 30%;
+  font-weight: 400;
+  font-size: 16px;
+  color: #adb5bd;
+`;
+
+const Divider = styled.div`
+  margin-top: 30px;
+  border-top: 1px solid #0e415a;
+`;
+
 function Footer() {
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <CompanyIcon src={DevkeyLogo} />
+      <Body>
+        Vi hjälper dig att förverkliga eller vidareutveckla ditt företags app
+        idé på ett konstadseffektivt sätt. Alla appar vi bygger skrivs i Googles
+        populära ramverk Flutter, som fungerar för både iOS & Android.
+      </Body>
+      <Divider />
+      <Body>© {new Date().getFullYear()} devkey Software</Body>
+    </Wrapper>
+  );
 }
 
 export default Footer;
